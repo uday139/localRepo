8c9654de17b3e3dbbfe0f90cf4b0b43fb595d618
uday139 <udayrajbhosle@gamil.com>
2026-09-11 12:20:32 +0530

    Added the java to the rep

--- new1.java
+++ new1.java
@@ -1,3 +1,15 @@
+import java.util.*;
 public class new1 {
-    
+    public static void main(String [] arg){
+        Scanner ne= new Scanner(System.in);
+        int []z = new int [5];
+        for(int i=0;i<5;i++){
+            z[i]= ne.nextInt();
+
+        }
+        Arrays.sort(z);
+        System.out.print(Arrays.toString(z));
+
+
+    }
 }
