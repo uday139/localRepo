cfb4a67c1ae873eb033cdddf370489a96ad7f4da
uday139 <udayrajbhosle@gamil.com>
2026-09-15 09:21:04 +0530

    Added things to java

--- java2.java
+++ java2.java
@@ -1,6 +1,12 @@
 public class java2 {
     public static void main (String [] a){
-        String q="Uday";
+        String q="Uday"; //this one is diffrrent than the bellow one 
+        q = "sachin"; // but the name is same for both
+        String abc=new String("sachin");
         System.out.println(q);
+        String ab ="sachin";
+        System.out.println(q==ab);
+        System.out.println(ab.equals(q));
+        System.out.println(q==abc);
     }
 }
