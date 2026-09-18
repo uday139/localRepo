9f1e1320fa84b8d7020f998d31039b932c632669
uday139 <udayrajbhosle@gamil.com>
2026-09-18 14:28:46 +0530

    Added it well

--- terniory.java
+++ terniory.java
@@ -1,7 +1,7 @@
 public class terniory {
     public static void main (String [] str){
         int a=10,b=3;
-       int c = (a>b) ? a: b;
-       System.out.println(c);
+        //int c = (a>b) ? a: b;
+       System.out.println((a>b) ? "The a var is greater: "+a: "The b var is greater: "+b);
     }
 }
